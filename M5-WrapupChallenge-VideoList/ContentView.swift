ce591ee7fae6a8770ab88aed2de768cd81326c9c
Frank J Lemur <frank.j.lemur@gmail.com>
2022-08-06 13:18:48 -0400

    Initial Commit

---
+++ M5-WrapupChallenge-VideoList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  M5-WrapupChallenge-VideoList
+//
+//  Created by Ian Howard on 8/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ M5-WrapupChallenge-VideoList/M5_WrapupChallenge_VideoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  M5_WrapupChallenge_VideoListApp.swift
+//  M5-WrapupChallenge-VideoList
+//
+//  Created by Ian Howard on 8/6/22.
+//
+
+import SwiftUI
+
+@main
+struct M5_WrapupChallenge_VideoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
